698cade3d962ffb4abeaeb8ea5755039f86b65aa
jamesthegr <jamesthegret1@gmail.com>
2026-03-28 17:16:07 +0800

    Update featured-games.tsx

--- components/featured-games.tsx
+++ components/featured-games.tsx
@@ -6,28 +6,28 @@ const games = [
     title: "Your Sus Gas Station Worker",
     description: "Work at the gas station in this interactive roleplay experience",
     image: "https://hebbkx1anhila5yf.public.blob.vercel-storage.com/image-MZuKFd1mgO6Op8qi5tmOMhPgqKHtPE.png",
-    redirectUrl: "https://roblox.com.ge/games/15932212022/your-sus-gas-station-worker?privateServerLinkCode=05206869975896966344753311133467",
+    redirectUrl: "https://www.robiox.com.py/games/15932212022/your-sus-gas-station-worker?privateServerLinkCode=439909404942835113814049442867",
   },
   {
     id: 2,
     title: "AI NPC Friends",
     description: "Chat and interact with AI-powered NPCs in this social experience",
     image: "https://hebbkx1anhila5yf.public.blob.vercel-storage.com/image-x7EBpStiVaadtzv33wnQ4skcdahzp9.png",
-    redirectUrl: "https://roblox.com.ge/games/13256679052/My-NPC-Friend?privateServerLinkCode=05206869975896966344753311133467",
+    redirectUrl: "https://www.robiox.com.py/games/102168567330694/Character-Talk?privateServerLinkCode=439909404942835113814049442867",
   },
   {
     id: 3,
     title: "Neko Girl Town",
     description: "Explore a vibrant town filled with neko characters and adventures",
     image: "https://hebbkx1anhila5yf.public.blob.vercel-storage.com/image-CJ47oBaCFWuH4nTgLT2V5bz3cZF20M.png",
-    redirectUrl: "https://roblox.com.ge/games/14153443454/Meet-a-neko?privateServerLinkCode=05206869975896966344753311133467",
+    redirectUrl: "https://www.robiox.com.py/games/126018438287036/Neko-Girl-Town?privateServerLinkCode=439909404942835113814049442867",
   },
   {
     id: 4,
     title: "Weird Neko Untitled Game",
     description: "A unique and mysterious neko-themed adventure experience",
     image: "https://hebbkx1anhila5yf.public.blob.vercel-storage.com/image-3KGj1YI9uE0ucJ823PWpM5e0xOvZPn.png",
-    redirectUrl: "https://www.roblox.com.ml/games/76106209840076/Weird-Neko-Untitled-Game?privateServerLinkCode=39422620633984157170371300026713",
+    redirectUrl: "https://www.robiox.com.py/games/76106209840076/Weird-Neko-Untitled-Game?privateServerLinkCode=439909404942835113814049442867",
   },
 ]
 
